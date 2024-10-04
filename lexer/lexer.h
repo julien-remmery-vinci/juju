fea8b6dcdeb42a81a476e2cce9e3dab252138afc
Julien Remmery <julien.remmery@student.vinci.be>
2024-10-04 13:17:50 +0200

    feat: add documentation to lexing functions

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -4,10 +4,24 @@
 #include <stdio.h>
 #include "../include/error.h"
 
+/*  
+    Create and return a new Tokens struct     
+*/  
 Tokens new_tokens();
 
+/*  
+    Args : FILE* opened with 'r' option
+    Return the next token found in file :
+        - NULL if no token found 
+*/
 char* next_token(FILE* file);
 
+/*  
+    Args : FILE* opened with 'r' option
+    Return Error struct with error type set to corresponding error :
+        - ARGS_ERROR : provided file is NULL 
+        - ERROR_NONE : 
+*/
 Error lex(FILE* file);
 
 #endif
